40323d3869abd53e6aa86c691adda435decb9e0c
Bekir Demircioglu <bkrdmrcioglu@gmail.com>
2026-07-14 16:04:55 +0300

    Bump to 0.2.6

--- Casks/devcheck.rb
+++ Casks/devcheck.rb
@@ -9,10 +9,10 @@
 #   https://github.com/bkrdmrcioglu/devcheck-site/releases/download/vX.Y.Z/DevCheck-X.Y.Z.zip
 
 cask "devcheck" do
-  version "0.2.5"
-  sha256 "0e080c4c5589b28e1c243de8d6436306e2f2c3b45fd0aab80d0f588868ea2574"
+  version "0.2.6"
+  sha256 "a3736c66addacca298c39ade94e011afe8291709e1b347f0c77aaf3e1d8a1d34"
 
-  url "https://github.com/bkrdmrcioglu/devcheck-site/releases/download/v0.2.5/DevCheck-0.2.5.zip"
+  url "https://github.com/bkrdmrcioglu/devcheck-site/releases/download/v0.2.6/DevCheck-0.2.6.zip"
   name "DevCheck"
   desc "Localhost HTTP header shield + App Store / Play prep"
   homepage "https://devsuites.dev/devcheck/"
